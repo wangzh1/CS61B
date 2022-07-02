2e256be54360943e98b61aa764e52665e1826705
ALANZW <743387789@qq.com>
2022-07-02 10:41:32 +0800

    NBody Finished

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -2,25 +2,63 @@ public class NBody {
 	
 	public static double readRadius (String file) {
 		In in = new In(file);
-		int first = in.readInt();
+		int N = in.readInt();
 		double second = in.readDouble();
 		return second;
 	}
 
 	public static Planet[] readPlanets (String file) {
 		In in = new In(file);
-		Planet[] allPlanets = new Planet[5];
-		int first = in.readInt();
+		int N = in.readInt();
+		Planet[] allPlanets = new Planet[N];
+
 		double second = in.readDouble();
-		for (int i = 0; i < 5; ++i) {
+		for (int i = 0; i < N; ++i) {
 			double arg1 = in.readDouble();
 			double arg2 = in.readDouble();
 			double arg3 = in.readDouble();
 			double arg4 = in.readDouble();
 			double arg5 = in.readDouble();
 			String arg6 = in.readString();
-			allPlanets[i] = new Planet(arg1, arg2, arg3, arg4, arg5, arg6);
+			allPlanets[i] = new Planet(arg1, arg2, arg3, arg4, arg5, "./images/"+arg6);
 		}
 		return allPlanets;
 	}
+
+	public static void main (String[] args) {
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		Planet[] allPlanets = readPlanets(filename);
+		double r = readRadius(filename);
+		StdDraw.setScale(-r, r);
+		String img = "./images/starfield.jpg";
+		StdDraw.enableDoubleBuffering();
+		int time = 0;
+		In in = new In(filename);
+		int N = in.readInt();
+
+		while (time != T) {
+			double[] xForces = new double[N];
+			double[] yForces = new double[N];
+			
+			for (int i = 0; i < N; ++i) {
+				xForces[i] = allPlanets[i].calcNetForceExertedByX(allPlanets);
+				yForces[i] = allPlanets[i].calcNetForceExertedByY(allPlanets);
+			}
+
+			for (int i = 0; i < N; ++i) {
+				allPlanets[i].update(dt, xForces[i], yForces[i]);
+			}
+			/* Draw the background image */
+			StdDraw.clear();
+			StdDraw.picture(0, 0, img);
+			for (Planet x : allPlanets) {
+				StdDraw.picture(x.xxPos, x.yyPos, x.imgFileName);
+			}
+			StdDraw.show();
+			StdDraw.pause(10);
+			time += dt;
+		}
+ 	}
 }
