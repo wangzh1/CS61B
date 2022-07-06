6ac005a85e62aed11666de84e6ee73c8d678c6eb
ALANZW <743387789@qq.com>
2022-07-06 15:22:09 +0800

    Tracking

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,10 +1,14 @@
 public class HorribleSteve {
     public static void main(String [] args) {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
-            if (!Flik.isSameNumber(i, j)) {
-                break; // break exits the for loop!
+        int j = 0;
+        while (true) {
+            if (i < 500) {
+                i++;
+            } else {
+                break;
             }
+            j++;
         }
         System.out.println("i is " + i);
     }
