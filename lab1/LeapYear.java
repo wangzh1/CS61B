4a66fdee8c5ada7b1435c05ec6e059f78d9961ca
ALANZW <743387789@qq.com>
2022-07-06 15:23:30 +0800

    Finished lab2 & 3

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,7 +2,15 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+    /** Check if 
+     * @param year is a leap year 
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        } else return false;
 
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
