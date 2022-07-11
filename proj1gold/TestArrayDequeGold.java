285e8ac7824cef806c0a2004f8d529459dbe19ec
ALANZW <743387789@qq.com>
2022-07-11 11:08:05 +0800

    Finished proj1gole

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class TestArrayDequeGold {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestArrayDequeGold {
+    @Test
+    public void testDequeRandomly() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StringBuilder tmp = new StringBuilder();
+        for (int i = 0; i < 12; ++i) {
+            int temp = StdRandom.uniform(10);
+            if (temp < 5) {
+                sad.addFirst(temp);
+                ads.addFirst(temp);
+                tmp.append("addFirst(").append(temp).append(")").append("\n");
+
+            } else {
+                sad.addLast(temp);
+                ads.addLast(temp);
+                tmp.append("addLast(").append(temp).append(")").append("\n");
+            }
+        }
+        for (int i = 0; i < 3; ++i) {
+            assertEquals(tmp + "removeFirst()\n", sad.removeFirst(), ads.removeFirst());
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            assertEquals(tmp + "removeLast()\n", sad.removeLast(), ads.removeLast());
+        }
+    }
 }
